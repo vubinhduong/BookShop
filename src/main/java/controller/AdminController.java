69e6dad48325447b91e6d4b44dede2c8029b8e92
vubinhduong <coder.tokyo@gmail.com>
2021-01-07 12:26:05 +0700

    update

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import model.Book;
@@ -12,13 +13,14 @@ import service.BookService;
 
 @Controller
 public class AdminController {
+	BookService bookService;
+	
 	@RequestMapping(value = "/adminPage", method = RequestMethod.GET)
 	public ModelAndView adminPage() {
 		ModelAndView mav = new ModelAndView("admin/index");
 		return mav;
 	}
-	
-	
+
 	@RequestMapping(value = "/dataBook", method = RequestMethod.GET)
 	public ModelAndView dataBookPage() {
 		ModelAndView mav = new ModelAndView("admin/tables");
@@ -26,7 +28,7 @@ public class AdminController {
 		mav.addObject("allBook", allBook);
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/dataBill", method = RequestMethod.GET)
 	public ModelAndView dataBookBill() {
 		ModelAndView mav = new ModelAndView("admin/tables2");
@@ -34,13 +36,27 @@ public class AdminController {
 		mav.addObject("allBook", allBook);
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/addBook", method = RequestMethod.GET)
 	public ModelAndView addBook() {
 		ModelAndView mav = new ModelAndView("admin/addBook");
 		return mav;
 	}
-	
+
+	@RequestMapping(value = "/addBookFill", method = RequestMethod.GET)
+	public ModelAndView addBookFill(@RequestParam(value = "bookId") String bookId,
+			@RequestParam(value = "bookName") String bookName, @RequestParam(value = "pages") int pages,
+			@RequestParam(value = "NXB") String NXB, @RequestParam(value = "nam") int nam,
+			@RequestParam(value = "price") int price, @RequestParam(value = "discount") int discount, @RequestParam(value = "author") String author,
+			@RequestParam(value = "image") String image, @RequestParam(value = "content") String content) {
+		if (bookService.checkBook(bookId)) {
+			return addBook();
+		} else {
+			bookService.addBook(bookId, bookName, pages, NXB, nam, price, discount, author, image, content);
+			return dataBookPage();
+		}
+	}
+
 	@RequestMapping(value = "/charts", method = RequestMethod.GET)
 	public ModelAndView chartsPage() {
 		ModelAndView mav = new ModelAndView("admin/charts");
